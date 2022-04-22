1a299b12a32367defc3a5333e50218d6e3df5b9b
mehmetelbeyli79 <mehmetelbeyli88@gmail.com>
2022-04-22 23:17:20 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [HttpGet]
+        public ActionResult HomePage()
+        {
+            return View();
+        }
+
+        // POST: Home
+        [HttpPost]
+        public ActionResult HomePage(string text1, bool check1, string list1)
+        {
+            var t1 = Request.Form["text1"];
+            var l1 = Request.Form["list1"];
+            var c1 = Request.Form.GetValues("check1")[0];
+            return View();
+        }
+    }
+}
